12a1855569375980b0d7e10d228dfea86c5131ac
zakiatulbalqis <zbalqis34@gmail.com>
2026-04-21 13:04:15 +0800

    perbarui

--- app.py
+++ app.py
@@ -45,5 +45,8 @@ def predict():
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
